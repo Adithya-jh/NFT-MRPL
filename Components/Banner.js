7bcb64d4f99919c986092b84e6451286e2c6d720
Adithya <jhadith88@gmail.com>
2023-04-28 16:11:47 +0530

    completed banner bruh

--- Components/Banner.js
+++ Components/Banner.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-const Banner = ({ parentStyles }) => {
+export default function Banner({ parentStyles, banner, childStyles }) {
   return (
     <div
-      className={`relative w-full flex items-center z-0 overflow-hidden nft-gradient ${parentStyles}`}
+      className="relative w-full flex  items-center z-0 overflow-hidden nft-gradient 
+      justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
     >
-      Bammer
+      <p
+        className={`font-bold ${childStyles} md:text-4xl sm:text-2xl xs:text-xl text-left text-5xl font-poppins leading-70`}
+      >
+        {banner}
+      </p>
+
+      <div className="absolute w-48 h-48 sm:w-32 sm:h-32 rounded-full white-bg -top-9 -left-16 -z-5" />
+      <div className="absolute w-72 h-72 sm:w-56 sm:h-56  rounded-full white-bg -bottom-24 -right-14 -z-5" />
     </div>
   );
-};
-
-export default Banner;
+}
