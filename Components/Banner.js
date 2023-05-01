824769d4d7d0c29b7ee04b1242e911a58b2030b4
Adithya <jhadith88@gmail.com>
2023-05-01 12:57:30 +0530

    done some things oncreatorcard

--- Components/Banner.js
+++ Components/Banner.js
@@ -3,12 +3,10 @@ import React from 'react';
 export default function Banner({ parentStyles, banner, childStyles }) {
   return (
     <div
-      className="relative w-full flex  items-center z-0 overflow-hidden nft-gradient 
+      className="relative w-full flex items-center z-0 overflow-hidden nft-gradient 
       justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
     >
-      <p
-        className={`font-bold ${childStyles} md:text-4xl sm:text-2xl xs:text-xl text-left text-5xl font-poppins leading-70`}
-      >
+      <p className="font-bold  md:text-4xl sm:text-2xl xs:text-xl text-left text-5xl font-poppins leading-70">
         {banner}
       </p>
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,28 @@ export default function Home() {
   const scrollRef = useRef(null);
   const [width, setWidth] = useState(0);
   const { theme, setTheme } = useTheme();
+
+  const handleScroll = (direction) => {
+    const [hideButton, setHideButton] = useState(false);
+    const { current } = scrollRef;
+
+    const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
+
+    if (direction === 'left') {
+      current.scrollLeft -= scrollAmount;
+    } else {
+      current.scrollLeft += scrollAmount;
+    }
+  };
+
+  const isScrollable = () => {
+    const { current } = scrollRef;
+    const { current: parent } = parentRef;
+
+    if (current?.scrollW) {
+    }
+  };
+
   return (
     <div className="flex justify-center sm:px-4 p-12 ">
       <div className="w-full minmd:w-4/5 ">
@@ -23,16 +45,13 @@ export default function Home() {
         />
 
         <div>
-          <h1
-            className="font-poppins dark:text-white text-nft-black-1 text-2xl
-           minlg:text-4xl font-semibold ml-4 xs:ml-0"
-          >
+          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
             Best Creators
           </h1>
-
-          <div className="relative flex-1 flex mt-3" ref={parentRef}>
+          {/* 
+          <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
             <div
-              className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
+              className="flex flex-row w-full overflow-x-scroll no-scrollbar select-none"
               ref={scrollRef}
             >
               {[6, 7, 8, 9, 10].map((i) => {
@@ -70,6 +89,53 @@ export default function Home() {
                   />
                 </div>
               </>
+            </div>
+          </div> */}
+
+          <div
+            className="relative flex  flex-1 max-w-full  mt-3"
+            ref={parentRef}
+          >
+            <div
+              className="flex flex-row w-max overflow-x-scroll scrollbar-hide select-none"
+              ref={scrollRef}
+            >
+              {[6, 7, 8, 9, 10].map((i) => (
+                <div key={i} className="flex-shrink-0">
+                  <CreatorCard
+                    rank={i}
+                    CreatorImage={images[`creator${i}`]}
+                    CreatorName={`0x${makeId(3)}...${makeId(4)}`}
+                    CreatorEths={10 - i * 0.5}
+                  />
+                </div>
+              ))}
+
+              {!hideButton && (
+                <>
+                  <div
+                    onClick={() => handleScroll('left')}
+                    className="absolute w-8 h-16 minlg:w-12 minlg:h-12 top-65 cursor-pointer left-0"
+                  >
+                    <Image
+                      src={images.left}
+                      alt="left"
+                      className={theme === 'light' ? 'filter invert' : ''}
+                    />
+                  </div>
+
+                  <div
+                    onClick={() => handleScroll('right')}
+                    className="relative w-8 h-16 minlg:w-12 minlg:h-12 top-65 cursor-pointer right-0"
+                  >
+                    <Image
+                      src={images.right}
+                      alt="right"
+                      className={theme === 'light' ? 'filter invert' : ''}
+                    />
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
