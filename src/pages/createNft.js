56c75b51061058f13e063fbd52df44b6de41b593
Adithya <jhadith88@gmail.com>
2023-05-06 21:32:24 +0530

    solved that fkn upload input problem

--- src/pages/createNft.js
+++ src/pages/createNft.js
@@ -16,9 +16,19 @@ const create_Nft = () => {
 
   const fileStyle = useMemo(
     () =>
-      'dark:bg-nft-black-1 p-5 bg-red border dark:border-red border-nft-gray-2 flex flex-col items-centerrounded-sm border-dashed',
+      'dark:bg-nft-black-1  bg-white border dark:border-red border-nft-gray-2 flex flex-col items-center p-5 rounded-sm border-dashed',
     []
   );
+
+  // const fileStyle2 = useMemo(
+  //   () =>
+  //     `'dark:bg-nft-black-1 bg-white border dark:border-red border-nft-gray-2 flex flex-row pt-5 pb-5 pr-5 items-center rounded-sm border-dashed'
+  //     ${isDragActive && 'border-file-active'}
+  //     ${isDragAccept && 'border-file-accept'}
+  //     ${isDragReject && 'border-file-reject'}
+  //     `,
+  //   [isDragActive, isDragAccept, isDragReject]
+  // );
 
   const onDrop = useCallback(() => {
     //upload image to ipfs
@@ -37,9 +47,19 @@ const create_Nft = () => {
     maxSize: 5000000,
   });
 
+  const fileStyle2 = useMemo(
+    () =>
+      `'dark:bg-nft-black-1 bg-white border dark:border-red border-nft-gray-2 flex flex-row pt-5 pb-5 pr-5 items-center rounded-sm border-dashed' 
+      ${isDragActive && 'border-file-active'}
+      ${isDragAccept && 'border-file-accept'}
+      ${isDragReject && 'border-file-reject'}
+      `,
+    [isDragActive, isDragAccept, isDragReject]
+  );
+
   return (
     <div className="flex justify-start align-middle sm:px-4 p-16">
-      <div className="w-3/5 md:w-full">
+      <div className="w-full">
         <h1 className="font-poppins dark:text-white mt-6 text-nft-black-1 text-xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
           CREATE YOUR OWN NFT
         </h1>
@@ -49,13 +69,18 @@ const create_Nft = () => {
             <p className="font-poppins dark:text-white mt-6 text-nft-black-1 font-semibold text-lg">
               UPLOAD FILE
             </p>
-            <div className="relative mt-6 w-full p-5 h-500">
+            <div className="relative mt-6 pb-5 pt-5 h-500">
               <div
                 {...getRootProps()}
-                className={fileStyle}
-                // className="dark:bg-nft-black-1 bg-white border dark:border-white border-nft-gray-2 flex flex-col items-center p-5 rounded-sm border-dashed"
+                className={fileStyle2}
+                // className="w-[50%] dark:bg-nft-black-1 bg-white border dark:border-white border-nft-gray-2 p-5 flex flex-row items-center rounded-sm border-dashed"
               >
                 <input {...getInputProps()} />
+                <div className="flexCenter flex-col text-center">
+                  <p className="font-poppins dark:text-white mt-6 text-nft-black-1 font-semibold text-lg">
+                    JPG,PNG,GIF,SVG,WEBM,MP3,MP4,
+                  </p>
+                </div>
               </div>
             </div>
           </div>
